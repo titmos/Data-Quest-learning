75446f3096026d8513be3894db471a2da127371f
titmos <37660314+titmos@users.noreply.github.com>
2024-10-08 17:38:20 -0700

    Update exploratory viz.py

--- exploratory viz.py
+++ exploratory viz.py
@@ -6,3 +6,18 @@ plt.barh(top20_deathtoll['Country_Other'], top20_deathtoll['Total_Deaths'])
 plt.show()
 
 
+#The OO Interface
+#we use the plt.subplots() function, which generates an empty plot and returns a tuple of two objects
+#The Figure (the canvas)
+#The Axes (the plot; don't confuse with "axis," which is the x- and y-axis of a plot).
+#
+
+import pandas as pd
+import matplotlib.pyplot as plt
+
+top20_deathtoll = pd.read_csv('top20_deathtoll.csv')
+
+fig, ax = plt.subplots()
+ax.barh(top20_deathtoll['Country_Other'], top20_deathtoll['Total_Deaths'])
+plt.show()
+
