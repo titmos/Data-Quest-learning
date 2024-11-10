ed2e13a423e3d08092c6bca4e838d704d2c84053
titmos <37660314+titmos@users.noreply.github.com>
2024-11-10 05:39:25 -0800

    Update probability_and_stat_py.py

--- probability_and_stat_py.py
+++ probability_and_stat_py.py
@@ -10,3 +10,21 @@ parameter = wnba['Games Played'].max() #This is a population metric
 sample = wnba.sample(n = 30, random_state = 1)#Returns a random sample of items from an axis of object.We can use random_state for reproducibility.
 statistic = sample['Games Played'].max() #This is the sample metric
 sampling_error = parameter - statistic
+
+#Simple Random Sampling
+import pandas as pd
+import matplotlib.pyplot as plt
+
+wnba = pd.read_csv('wnba.csv')
+
+population_mean = wnba['PTS'].mean()
+sample_means = []
+#Using simple random sampling, take 100 samples of 10 values each from our WNBA data set, and for each sample, measure the average points scored by a player during the 2016-2017 season.
+for item in range (100):
+    sample = wnba['PTS'].sample(n = 10, random_state = item)
+    sample_means.append(sample.mean())
+    
+#Display the discrepancy between the parameter of interest (the mean of the PTS column) and the statistics obtained in the previous step
+plt.scatter(range(1, 101), sample_means)
+plt.axhline(population_mean)
+plt.show()
