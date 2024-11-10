9246d64ceb5424dda44b760cad73e8ca8b4b8dd2
titmos <37660314+titmos@users.noreply.github.com>
2024-11-10 03:47:13 -0800

    Update probability_and_stat_py.py

--- probability_and_stat_py.py
+++ probability_and_stat_py.py
@@ -1,1 +1,12 @@
-#...
+
+#Sampling Error
+
+import pandas as pd
+wnba = pd.read_csv('wnba.csv')
+print(wnba.shape) #Find the number of rows and columns
+#print(wnba.head()) #Check  first five rows
+#print(wnba.tail()) #Check last five rows
+parameter = wnba['Games Played'].max() #This is a population metric
+sample = wnba.sample(n = 30, random_state = 1)#Returns a random sample of items from an axis of object.We can use random_state for reproducibility.
+statistic = sample['Games Played'].max() #This is the sample metric
+sampling_error = parameter - statistic
