f022703175a0b590c93b254c1decac01bdda0219
titmos <37660314+titmos@users.noreply.github.com>
2024-11-10 11:49:00 -0800

    Update probability_and_stat_py.py

--- probability_and_stat_py.py
+++ probability_and_stat_py.py
@@ -30,3 +30,23 @@ plt.axhline(population_mean)
 plt.show() #On the scatter plot from the previous screen, we can see that the sample means vary a lot around the population mean.
 # there's a good chance we get a sample that isn't representative of the population
 #We can solve this problem by increasing the sample size. As we increase the sample size, the sample means vary less around the population mean, and the chances of getting an unrepresentative sample decrease.
+
+
+#Stratified Sampling
+wnba['Pts_per_game'] = wnba['PTS'] / wnba['Games Played'] #creates a new column for points/game
+print(wnba['Pos'].unique()) #to view the strata in the Pos column
+
+# Stratifying the position data in five strata using boolean 
+stratum_G = wnba[wnba.Pos == 'G']
+stratum_F = wnba[wnba.Pos == 'F']
+stratum_C = wnba[wnba.Pos == 'C']
+stratum_GF = wnba[wnba.Pos == 'G/F']
+stratum_FC = wnba[wnba.Pos == 'F/C']
+
+points_per_position = {} #Empty dictionary to save sample mean per stratum
+#Loop to iterate the stratum(done by boolean filter)
+for stratum, position in [(stratum_G, 'G'), (stratum_F, 'F'), (stratum_C, 'C'), (stratum_GF, 'G/F'), (stratum_FC, 'F/C')]:
+    sample = stratum['Pts_per_game'].sample(n = 10, random_state = 0)## simple random sampling on each stratum
+    points_per_position[position] = sample.mean() #appends mean of each strata tp the dictionary by its position 
+    
+position_most_points = max(points_per_position, key = points_per_position.get)#To get the strata with Max points 
