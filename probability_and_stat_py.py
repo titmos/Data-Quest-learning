57c84e3b5284a0a9942cb83b0ca467beb2f191c3
titmos <37660314+titmos@users.noreply.github.com>
2024-11-19 15:32:44 -0800

    Update probability_and_stat_py.py

--- probability_and_stat_py.py
+++ probability_and_stat_py.py
@@ -110,3 +110,9 @@ age_descending = wnba['Age'].value_counts().sort_index(ascending = False)
 #wnba['Height'].value_counts() returns a Series object with the measures of height as indices. This allows us to sort the table by index using the Series.sort_index() method:
 print(wnba['Height'].value_counts().sort_index())
 #We can also sort the table by index in a descending order using wnba['Height'].value_counts().sort_index(ascending = False)
+
+
+#Proportions and percentages
+#It's slightly faster though to use Series.value_counts() with the normalize parameter set to True 
+print(wnba['Pos'].value_counts(normalize = True))
+#To find percentages, we just have to multiply the proportions by 100:
