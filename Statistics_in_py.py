87ce8171f01ac3a1b86ffb0bd20db0c39d3307bc
titmos <37660314+titmos@users.noreply.github.com>
2024-12-09 17:58:35 -0800

    Update Statistics_in_py.py

--- Statistics_in_py.py
+++ Statistics_in_py.py
@@ -664,3 +664,23 @@ bedroom_mode = houses['Bedroom AbvGr'].mode()
 
 print(houses['SalePrice'].value_counts().sort_index())
 price_variable = 'continuous'
+
+#grouped frequency table for the SalePrice
+intervals = pd.interval_range(start=0, end=800000, freq=100000)
+gr_freq_table = pd.Series([0, 0, 0, 0, 0, 0, 0, 0], index=intervals)
+
+for value in houses['SalePrice']:
+    for interval in intervals:
+        if value in interval:
+            gr_freq_table.loc[interval] += 1
+            break
+
+maxi = max(gr_freq_table.values)
+
+print(gr_freq_table)
+mode = 150000
+mean = houses['SalePrice'].mean()
+median = houses['SalePrice'].median()
+sentence_1 = True
+
+sentence_2 = True
