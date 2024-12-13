06c9c55a4d239ef3bf152c04f6f9aa68729cf6fb
titmos <37660314+titmos@users.noreply.github.com>
2024-12-13 13:47:09 -0800

    Update Statistics_in_py.py

--- Statistics_in_py.py
+++ Statistics_in_py.py
@@ -849,3 +849,25 @@ greatest_variability = max(dict, key= lambda x: dict[x]) #or try  max(years, key
 lowest_variability = min(dict, key= lambda x: dict[x]) #or try  min(years, key=years.get)
 print(dict)
 #print(houses['SalePrice'][houses['Yr Sold']==2009])
+
+
+#A Measure of Spread
+def standard_deviation(array):
+    reference_point = sum(array) / len(array)
+    
+    distances = []
+    for value in array:
+        squared_distance = (value - reference_point)**2
+        distances.append(squared_distance)
+    
+    variance = sum(distances) / len(distances)
+    
+    return sqrt(variance)
+
+
+sample1 = houses['Year Built'].sample(50, random_state=1)
+sample2 = houses['Year Built'].sample(50, random_state=2)
+bigger_spread = 'sample 2'
+st_dev1 = standard_deviation(sample1)
+st_dev2 = standard_deviation(sample2)
+
